684cf1c8020b34a5ab88a8ae6de7b9a53f857265
Jesse <Feine@live.com>
2017-02-27 02:45:16 -0600

    NEW BACKGROUND ADDED "PIXEL PERFECT" CAMERA LOOPING CLOUDS
    the original file is under "HABITAT 1.unity" and contains the original camera.

---
+++ Assets/CloudLooping.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudLooping : MonoBehaviour {
+
+	public float speed =0;
+	public Vector3 startPOS;
+
+
+	void FixedUpdate () 
+	{
+
+		transform.position =Vector3.MoveTowards (transform.position, new Vector3 (12.65f,
+			this.transform.position.y,
+			this.transform.position.z),
+			speed * Time.deltaTime);
+
+		if (transform.position.x < -2.65f) {
+			transform.position = new Vector3 (2.65f, this.transform.position.y, this.transform.position.z);
+		}
+	}
+}
